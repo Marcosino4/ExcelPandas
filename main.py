7dc1b3b0587d3d50905e1f181172968512318a03
Marcosino4 <marcosino654@gmail.com>
2024-02-13 23:22:00 +0100

    Panda no funca

---
+++ main.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import json
+from datetime import date
+from openpyxl import Workbook
+
+def load_employees_data(file_path):
+    with open(file_path) as employeesJson:
+        employees_data = json.load(employeesJson)
+    return employees_data
+
+def filter_employees(employees_data, project_name):
+    valid_employees = []
+    for employee in employees_data:
+        if employee['proyect'] == project_name:
+            valid_employees.append(employee)
+    return valid_employees
+
+def process_employees_salaries(employees):
+    for employee in employees:
+        employee['salary'] = f"{employee['salary'][1:]}"
+        if employee['age'] < 30:
+            employee['salary'] = f"{float(employee['salary'][:-1]) * 1.10:.2f}€"
+
+def save_dataframe_to_excel(df, excel_name):
+    with pd.ExcelWriter(excel_name, engine="openpyxl") as writer:
+        df.to_excel(writer, index=False)
+
+if __name__ == "__main__":
+    employees_data = load_employees_data("employees.json")
+    valid_employees = filter_employees(employees_data, "GRONK")
+    process_employees_salaries(valid_employees)
+    df = pd.DataFrame(valid_employees)
+    hoy = date.today()
+    fecha = f"{hoy.day}-{hoy.month}-{hoy.year}"
+    excel_name = f"pagos-empleados-{fecha}.xlsx"
+    save_dataframe_to_excel(df, excel_name)
